ac3c9f36b3b932ef91d4c4251c095df69e3c16e6
Mehdi <mjorshari@jaycar.com.au>
2019-10-18 16:01:52 +1100

    Add layout to html

--- src/main/java/au/com/jaycar/controller/UserController.java
+++ src/main/java/au/com/jaycar/controller/UserController.java
@@ -2,10 +2,14 @@ package au.com.jaycar.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import au.com.jaycar.business.UserBusiness;
 import au.com.jaycar.dto.UserDetailsDto;
@@ -17,20 +21,24 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/users")
 public class UserController {
-	
+
 	private final UserBusiness userBusiness;
 
 	@GetMapping("/list")
 	public String listUsers(Model model) {
-		
+
 		List<UserDetailsDto> detailsDtos = userBusiness.listAllUsers();
-		
+
 		log.info("retreived users: {}", detailsDtos);
-		
+
 		model.addAttribute("users", detailsDtos);
 
 		return "users/list";
 	}
-	
-	
+
+	@PostMapping("create")
+	public String createNewUser(@RequestParam @Valid UserDetailsDto detailsDto, Model model) {
+		return "redirect:/users/list";
+	}
+
 }
