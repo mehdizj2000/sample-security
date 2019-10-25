a43bf58707377e7acf2614f6040354715fb60320
mehdizj2000 <zareimeh@gmail.com>
2019-10-25 17:22:19 +1100

    Add extra fields to UserDetails. Add email confirmation validator

--- src/main/java/au/com/jaycar/dto/UserDetailsDto.java
+++ src/main/java/au/com/jaycar/dto/UserDetailsDto.java
@@ -6,20 +6,33 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Null;
 
+import au.com.jaycar.validation.annotation.EmailMatcher;
 import au.com.jaycar.validation.annotation.PasswordMatcher;
 import lombok.Data;
 
 @Data
 @PasswordMatcher
+@EmailMatcher
 public class UserDetailsDto {
 	
 	private Long id;
 	
 	@NotBlank
 	private String userName;
+	
+	@NotBlank
+	private String firstName;
+	
+	@NotBlank
+	private String lastName;
 
 	@Email
+	@NotBlank
 	private String email;
+	
+	@Email
+	@NotBlank
+	private String emailConfirmation;
 
 	@NotBlank
 	private String password;
---
+++ src/main/java/au/com/jaycar/validation/EmailMatcherValidator.java
@@ -0,0 +1,19 @@
+package au.com.jaycar.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.apache.commons.lang3.StringUtils;
+
+import au.com.jaycar.dto.UserDetailsDto;
+import au.com.jaycar.validation.annotation.PasswordMatcher;
+
+public class EmailMatcherValidator implements ConstraintValidator<PasswordMatcher, Object> {
+
+	@Override
+	public boolean isValid(Object value, ConstraintValidatorContext context) {
+		UserDetailsDto detailsDto = (UserDetailsDto) value;
+		return StringUtils.compare(detailsDto.getPassword(), detailsDto.getConfirmPassword()) == 0;
+	}
+
+}
